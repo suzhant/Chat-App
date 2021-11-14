e61f5e8b6f1252aec506f8445bc665298ae91119
suzhant <xresthasushant61@gmail.com>
2021-11-14 15:27:09 +0545

    changed notification sound

--- app/src/main/java/com/sushant/whatsapp/FirebaseMessagingService.java
+++ app/src/main/java/com/sushant/whatsapp/FirebaseMessagingService.java
@@ -6,6 +6,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -34,17 +36,17 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         Log.d("FCM", "onMessageReceived: "+remoteMessage.getNotification().getBody());
 
 // playing audio and vibration when user see request
-        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.iphone;
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), Uri.parse(path));
         r.play();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             r.setLooping(false);
         }
 
 //         vibration
-        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        long[] pattern = {100, 300, 300, 300};
-        v.vibrate(pattern, -1);
+//        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+//        long[] pattern = {100, 300, 300, 300};
+//        v.vibrate(pattern, -1);
 
         int resourceImage = getResources().getIdentifier(remoteMessage.getNotification().getIcon(), "drawable", getPackageName());
 
@@ -64,13 +66,13 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         Intent resultIntent = new Intent(this, MainActivity.class);
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, resultIntent, 0);
         builder.setContentTitle(remoteMessage.getNotification().getTitle());
         builder.setContentText(remoteMessage.getNotification().getBody());
         builder.setContentIntent(pendingIntent);
         builder.setStyle(new NotificationCompat.BigTextStyle().bigText(remoteMessage.getNotification().getBody()));
         builder.setAutoCancel(true);
         builder.setPriority(Notification.PRIORITY_MAX);
+        builder.setSound(Uri.parse(path),AudioManager.STREAM_NOTIFICATION);
 
         mNotificationManager =
                 (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
@@ -80,11 +82,16 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
         {
+//            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+//                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+//                    .setUsage(AudioAttributes.USAGE_ALARM)
+//                    .build();
             String channelId = "Your_channel_id";
             NotificationChannel channel = new NotificationChannel(
                     channelId,
                     "Channel human readable title",
                     NotificationManager.IMPORTANCE_HIGH);
+//            channel.setSound(Uri.parse(path),audioAttributes);
             mNotificationManager.createNotificationChannel(channel);
             builder.setChannelId(channelId);
         }
