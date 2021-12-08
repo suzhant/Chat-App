f7818d7870cc7aad108e8a0c977735c6f787e0f6
suzhant <xresthasushant61@gmail.com>
2021-12-08 13:16:49 +0545

    fixed group receiver box and changed bg color of chat room

--- app/src/main/java/com/sushant/whatsapp/Adapters/GroupChatAdapter.java
+++ app/src/main/java/com/sushant/whatsapp/Adapters/GroupChatAdapter.java
@@ -99,10 +99,10 @@ public class GroupChatAdapter extends RecyclerView.Adapter {
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (((ReceiverViewHolder) holder).txtSenderName.getVisibility()==View.VISIBLE){
-                        ((ReceiverViewHolder) holder).txtSenderName.setVisibility(View.GONE);
+                    if (((ReceiverViewHolder) holder).txtReceiverTime.getVisibility()==View.VISIBLE){
+                        ((ReceiverViewHolder) holder).txtReceiverTime.setVisibility(View.GONE);
                     }else {
-                        ((ReceiverViewHolder) holder).txtSenderName.setVisibility(View.VISIBLE);
+                        ((ReceiverViewHolder) holder).txtReceiverTime.setVisibility(View.VISIBLE);
                     }
                 }
             });
